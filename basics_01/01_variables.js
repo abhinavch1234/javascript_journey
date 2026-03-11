577503a10c4da25371b0c64e3f5d7a2a35bcdf72
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-03-11 17:24:48 +0000

    name changed

---
+++ basics_01/01_variables.js
@@ -0,0 +1,13 @@
+const account_ID = 144553
+let accountEmail = "abhinav@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+// account_ID = 2
+accountEmail = "hc@gmail.com"
+accountPassword = "121231"
+accountCity = "bengaluru"
+
+console.log(account_ID);
+
+console.table([account_ID,accountEmail,accountPassword,accountCity,accountState]);
