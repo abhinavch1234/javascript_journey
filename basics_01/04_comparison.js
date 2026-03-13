e1a0a6148adf0baebd1492f1fe691cca6aef3a29
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-03-13 16:46:10 +0000

    comparisons done

---
+++ basics_01/04_comparison.js
@@ -0,0 +1,20 @@
+//console.log(null>0);
+//console.log(null==0)
+//console.log(null>=0);
+
+//console.log(undefined ==0);
+//console.log(undefined<0);
+//console.log(undefined>0);
+
+// ===
+
+console.log("2" === 2);
+
+console.log("abhinav"== "abhinav" );
+
+
+
+
+
+
+
