4529a5b8c720b810921bf6c576f456415ac0c26d
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-03-13 16:27:36 +0000

    conversionoperations done

--- basics_01/03_conversionOperation.js
+++ basics_01/03_conversionOperation.js
@@ -1,27 +1,56 @@
-let score = true
+//let score = true
 
-console.log(typeof(score));
+//console.log(typeof(score));
 
-let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // "33"=> 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "abhinav"
-let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let isLoggedIn = "abhinav"
+//let booleanIsLoggedIn = Boolean (isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 // 1=> true : 0 => false 
 // "" => false
 // "abhinav" => true 
 
-let someNumber = 33
+//let someNumber = 33
 
-let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+//let StringNumber = String(someNumber)
+//console.log(StringNumber);
+//console.log(typeof StringNumber);
 
+// ************** Operations ***************
+
+let value = 3
+
+let negValue = -value
+
+console.log(negValue);
+
+let str1 = "abhinav"
+let str2 = " cool"
+
+let str3 = str1 + str2
+
+console.log(true);
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+  
