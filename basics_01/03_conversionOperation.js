803c6f43508f2dd69dfc5dc5e8283503c8e5d041
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-03-13 15:28:22 +0000

    conversion operations done

---
+++ basics_01/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "abhinav"
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true : 0 => false 
+// "" => false
+// "abhinav" => true 
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
