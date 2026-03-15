b8859cabb6b68f42c5f7fa91c73b183565cd7b0a
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-03-15 17:00:11 +0000

    strings done

---
+++ basics_01/05_strings.js
@@ -0,0 +1,44 @@
+const name = "abhinav"
+const repoCount = 50
+
+//console.log(name + repoCount + "value")
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("abhinav-hc-com")
+
+//console.log(gameName[0])
+//console.log(gameName.__proto__)
+
+
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+console.log( gameName.charAt(2));
+console.log( gameName.indexOf('a'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-10,4)
+console.log(anotherString)
+
+
+const newStringOne = "     abhinav      "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+
+const url = "https://abhinav.com/abhinav%20chauhdary"
+
+
+//console.log(url.replace("%20", "-"))
+
+//console.log(url.includes('abhinav'))
+
+console.log(gameName.split("-"))
+
+
+
+
