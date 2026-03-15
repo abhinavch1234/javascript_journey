020830ae61cb74fd1fc4b885d098300551aad39f
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-03-15 21:32:33 +0000

    arrays

---
+++ basics_02/01_arrays.js
@@ -0,0 +1,31 @@
+// array
+
+const myArr = [1, 2, 3, 4, 5]
+
+const myHeroes = ["shaktiman","naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+
+// ARRAY make shallow copy (copy whose property share same reference point)
+//console.log(myArr[1])
+
+// Array Methods
+
+
+//myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+
+console.log(typeof newArr)
+
+
+
+
