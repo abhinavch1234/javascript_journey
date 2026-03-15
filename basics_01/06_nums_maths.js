0e171ce2ef626207b8ccfa59cb1ab84d4ed9bc93
Abhinav Chaudhary <67009976+abhinavch1234@users.noreply.github.com>
2026-03-15 17:00:31 +0000

    maths and nums done

---
+++ basics_01/06_nums_maths.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2))
+
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4))
+
+const hundereds = 1000000
+//console.log(hundereds.toLocaleString('en-IN'))
+
+
+
+//==================== Maths ===================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(64));
+// console.log(Math.min(1,4,5,5,6,2 ,2, 37,2,237,7));
+// console.log(Math.max(25465,353,3442,524,65254,625,3535,26,26,762,27));
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min = 10 
+
+const max = 20
+
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
+ 
